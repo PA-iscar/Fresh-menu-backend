6c51bd765b8b049bd80b73102e6ab858f5570e2d
Abhishek <abhiaro27@gmail.com>
2022-03-29 21:36:33 +0530

    login and signup completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user.model");
 const Cart = require("../models/cart.model");
+const newOTP = require("otp-generators");
 
 const crudController = require("./crud.controller");
 const router = express.Router();
@@ -9,9 +10,14 @@ const controller = crudController(User);
 
 //* Create new User
 router.post("/register", async (req, res) => {
-  try {
+  const user1 = await User.find({ mobileNumber: req.body.mobileNumber });
+  const user2 = await User.find({ email: req.body.email });
+  if (user1.length > 0) {
+    res.status(400).json("");
+  } else if (user2.length > 0) {
+    res.status(400).json("");
+  } else {
     const createdUser = await User.create(req.body);
-    console.log(createdUser);
     const createdCart = await Cart.create({
       userID: createdUser["_id"],
       meals: [],
@@ -23,22 +29,44 @@ router.post("/register", async (req, res) => {
         new: true,
       }
     );
-    res.status(201).send("Registered successfully");
-  } catch (err) {
-    res.status(400).send("Bad request", err);
+    res.status(201).json("Registered successfully");
   }
 });
 
-// //* Read Many
-// router.get("/", controller.getAll);
+//* Check if user Exists if yes send OTP and ID
+router.get("/", async (req, res) => {
+  const user = await User.find(req.query);
+  if (user.length == 0) {
+    res.status(400).json("");
+  } else {
+    const OTP = newOTP.generate(5, {
+      alphabets: false,
+      upperCase: false,
+      specialChar: false,
+    });
+    const response = {
+      id: user[0]["_id"],
+      OTP,
+    };
+    const updatedUser = await User.findByIdAndUpdate(
+      user[0]["_id"],
+      { OTP },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(response);
+  }
+});
 
-// //* Read One
-// router.get("/:id", controller.getOne);
-
-// //* Update
-// router.patch("/:id", controller.updateOne);
-
-// //* Delete
-// router.delete("/:id", controller.deleteOne);
+//* Login user
+router.get("/login/:id/:otp", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user.OTP === req.params.otp) {
+    res.status(200).json(user);
+  } else {
+    res.status(400).json("");
+  }
+});
 
 module.exports = router;
