b7acbafdbd9c4150f6bb522f72265834eec005b0
Abhishek <abhiaro27@gmail.com>
2022-03-31 05:08:04 +0530

    added get request for all filters

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -9,6 +9,56 @@ const controller = crudController(Meal);
 //* Read Many
 router.get("/", controller.getAll);
 
+//* Get all Filters
+router.get("/filters", async (req, res) => {
+  const data = await Meal.find().select("type cuisine trending category -_id");
+  const filters = {};
+  const types = {};
+  const cuisines = {};
+  const trendings = {};
+  const categories = {};
+  for (let el of data) {
+    types[el.type] ? types[el.type]++ : (types[el.type] = 1);
+    cuisines[el.cuisine] ? cuisines[el.cuisine]++ : (cuisines[el.cuisine] = 1);
+    trendings[el.trending]
+      ? trendings[el.trending]++
+      : (trendings[el.trending] = 1);
+    categories[el.category]
+      ? categories[el.category]++
+      : (categories[el.category] = 1);
+  }
+
+  let i = 0;
+  let filters2 = [];
+  for (let key in types) {
+    filters2[i++] = { [key]: types[key] };
+  }
+  filters["types"] = filters2;
+
+  i = 0;
+  filters2 = [];
+  for (let key in cuisines) {
+    filters2[i++] = { [key]: cuisines[key] };
+  }
+  filters["cuisines"] = filters2;
+
+  i = 0;
+  filters2 = [];
+  for (let key in trendings) {
+    filters2[i++] = { [key]: trendings[key] };
+  }
+  filters["trendings"] = filters2;
+
+  i = 0;
+  filters2 = [];
+  for (let key in categories) {
+    filters2[i++] = { [key]: categories[key] };
+  }
+  filters["categories"] = filters2;
+
+  res.status(200).json(filters);
+});
+
 //* Read One
 router.get("/:id", controller.getOne);
 
